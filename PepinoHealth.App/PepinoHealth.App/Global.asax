c427ddc34425db47a524b4eb043e4e69419a81b7
PepinoHealth <pepinohealth.app@gmail.com>
2022-07-29 00:24:58 +0530

    Update .gitignore

--- PepinoHealth.App/PepinoHealth.App/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PepinoHealth.App.MvcApplication" Language="C#" %>
--- PepinoHealth.App/PepinoHealth.App/Scripts/dynamic/export.js
+++
@@ -1,8 +0,0 @@
-﻿~/Scripts/datatable/dataTables.rowGroup.min.js
-~/Scripts/datatable/dataTables.buttons.min.js
-~/Scripts/datatable/buttons.flash.min.js
-~/Scripts/datatable/jszip.min.js
-~/Scripts/datatable/pdfmake.min.js
-~/Scripts/datatable/vfs_fonts.min.js
-~/Scripts/datatable/buttons.html5.min.js
-~/Scripts/datatable/buttons.print.min.js
